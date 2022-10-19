b61b875c46b26259044b32171991a0681963ee15
madhavanvv <madhavan1177@gmail.com>
2022-10-19 12:30:48 +0530

    promise-concet-learn-1

--- concepts/myPromise.js
+++ concepts/myPromise.js
@@ -1,1 +1,28 @@
-console.log("MyPromise");
+// learn Promise
+
+const one = () => {
+  return "111111111";
+};
+const two = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("222222222");
+    }, 3000);
+  });
+};
+
+const three = () => {
+  console.log("hello promise testing");
+  return "33333333";
+};
+const myPromise = async () => {
+  let a = one();
+  console.log(a);
+
+  let b = await two();
+  console.log(b);
+
+  let c = three();
+  console.log(c);
+};
+myPromise();
